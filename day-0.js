bc9ec24754974489c3b63695550fddc2c376b5c5
jz <jz@jz-g6>
2014-11-30 18:16:54 +0400

    day 1 re-formating

--- day-0.js
+++ day-0.js
@@ -1,24 +1,24 @@
 function countWordStatistic(originalString) {
-	var filteredString = originalString.replace(/[\.\,\(\)\:\;\!\?\n]/g, ' ');
-	var compactedString = filteredString.replace(/\s+/g, ' ');
-	var trimmedString = compactedString.trim();
+    var filteredString = originalString.replace(/[\.\,\(\)\:\;\!\?\n]/g, ' ');
+    var compactedString = filteredString.replace(/\s+/g, ' ');
+    var trimmedString = compactedString.trim();
 
-	var wordsCountBySize = [];
-	var totalWordsCount = 0;
+    var wordsCountBySize = [];
+    var totalWordsCount = 0;
 
-	var words = trimmedString.split(' ');
+    var words = trimmedString.split(' ');
 
-	words.forEach(function(word) {
+    words.forEach(function(word) {
         var len = word.length;
         wordsCountBySize[len] = wordsCountBySize[len] + 1 || 1;
         totalWordsCount++;
-	});
+    });
 
-	for (var size in wordsCountBySize) {
-		var count = wordsCountBySize[size];
-		var percentage = ((count / totalWordsCount) * 100).toFixed(1);
-		console.log('' + size + ' letter(s) : ' + count + ' word(s) (' + percentage + '%)')
-	}
+    for (var size in wordsCountBySize) {
+        var count = wordsCountBySize[size];
+        var percentage = ((count / totalWordsCount) * 100).toFixed(1);
+        console.log('' + size + ' letter(s) : ' + count + ' word(s) (' + percentage + '%)');
+    }
 }
 
 countWordStatistic('Hello World');
