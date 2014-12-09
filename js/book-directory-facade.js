1ce2118786e3248eba038d6b356e4fa39f9e7115
Sergey Zheznyakovskiy <Sergey_Zheznyakovskiy@epam.com>
2014-12-09 21:49:29 +0400

    Day-4: Facade module extraction

---
+++ js/book-directory-facade.js
@@ -0,0 +1,192 @@
+define([], function() {
+    "use strict";
+
+    function BookDirectoryFacade(bookStore) {
+        var self = this;
+        self._bookStore = bookStore;
+
+        self._bookStore.getEventEmitter().subscribe('book-added', function(book){
+            self.appendBook(book);
+        });
+        self._bookStore.getEventEmitter().subscribe('book-deleted', function(book){
+            self.removeBook(book);
+        });
+    }
+
+    var _bdp = BookDirectoryFacade.prototype;
+
+    _bdp.cleanNode = function(node) {
+        while (node.hasChildNodes()) {
+            node.removeChild(node.lastChild);
+        }
+    };
+
+    _bdp.getBookListNode = function() {
+        return document.getElementById('book-list');
+    };
+
+    _bdp.getBookNode = function(bookId) {
+        return document.getElementById('book-container-' + bookId);
+    };
+
+    _bdp.showBookList = function() {
+        var self = this;
+        var bookListNode = self.getBookListNode();
+        self.cleanNode(bookListNode);
+        var books = self._bookStore.getAllBooks();
+        for (var bookIndex in books) {
+            var book = books[bookIndex];
+            var bookNode = self.generateBookNode(book);
+            bookListNode.appendChild(bookNode);
+        } 
+    };
+
+    _bdp.appendBook = function(book) {
+        var self = this;
+        var bookNode = self.generateBookNode(book);
+        self.getBookListNode().appendChild(bookNode);
+    };
+
+    _bdp.removeBook = function(book) {
+        var self = this;
+        self.getBookListNode().removeChild(self.getBookNode(book.id));
+    };
+
+    _bdp.setBookTitle = function(bookId, newTitle) {
+        var self = this;
+        if (newTitle.trim() === '') {
+            alert('New name cannot be empty');
+            return;
+        }    
+        if (!self._bookStore.bookNameIsAvailable(newTitle.trim(), bookId)) {
+            alert('New name should not conflict with other books');
+            return;
+        }    
+        var book = self._bookStore.getBookById(bookId);
+        book.name = newTitle;
+        self.switchToDisplayMode(bookId);
+    };
+
+    _bdp.switchToEditMode = function(bookId) {
+        var self = this;
+        var bookNode = self.getBookNode(bookId);
+        self.cleanNode(bookNode);
+        var book = self._bookStore.getBookById(bookId);
+        var editableGuts = self.generateEditableGuts(book);
+        bookNode.appendChild(editableGuts);
+    };
+
+    _bdp.switchToDisplayMode = function(bookId) {
+        var self = this;
+        var bookNode = self.getBookNode(bookId);
+        self.cleanNode(bookNode);
+        var book = self._bookStore.getBookById(bookId);
+        var displayableGuts = self.generateDisplayableGuts(book);
+        bookNode.appendChild(displayableGuts);
+    };
+
+    _bdp.switchToDeleteMode = function(bookId) {
+        var self = this;
+        var bookNode = self.getBookNode(bookId);
+        self.cleanNode(bookNode);
+        var book = self._bookStore.getBookById(bookId);
+        var deletableGuts = self.generateDeletableGuts(book);
+        bookNode.appendChild(deletableGuts);
+    };
+
+    _bdp.generateBookNode = function(book) {
+        var self = this;
+        var bookContainer = document.createElement('li');
+        bookContainer.setAttribute('id', 'book-container-' + book.id);
+        var displayableGuts = self.generateDisplayableGuts(book);
+        bookContainer.appendChild(displayableGuts);
+        return bookContainer;
+    };
+
+    _bdp.generateDisplayableGuts = function(book) {
+        var self = this;
+        var containerNode = self.renderTemplate('tmpl-book-display', book);
+        containerNode.querySelector('.button-edit').addEventListener('click', function(e) {
+            var bookId = parseInt(e.srcElement.dataset.bookId);
+            self.switchToEditMode(bookId);
+        });
+        containerNode.querySelector('.button-delete').addEventListener('click', function(e) {
+            var bookId = parseInt(e.srcElement.dataset.bookId);
+            self.switchToDeleteMode(bookId);
+        });
+        return containerNode;
+    };
+
+    _bdp.generateEditableGuts = function(book) {
+        var self = this;
+        var containerNode = self.renderTemplate('tmpl-book-edit', book);
+        var bookName = containerNode.querySelector('.input-book-new-name');
+        containerNode.querySelector('.form-book-edit').addEventListener('submit', function(e) {
+            e.preventDefault();
+            var bookId = parseInt(e.srcElement.dataset.bookId);
+            self.setBookTitle(bookId, bookName.value);
+        });
+        containerNode.querySelector('.button-edit-cancel').addEventListener('click', function(e) {
+            var bookId = parseInt(e.srcElement.dataset.bookId);
+            self.switchToDisplayMode(bookId);
+        });
+        return containerNode;
+    };
+
+    _bdp.generateDeletableGuts = function(book) {
+        var self = this;
+        var containerNode = self.renderTemplate('tmpl-book-delete', book);
+        containerNode.querySelector('.button-delete-confirm').addEventListener('click', function(e) {
+            var bookId = parseInt(e.srcElement.dataset.bookId);
+            var book = self._bookStore.getBookById(bookId);
+            self._bookStore.removeBook(book);
+        });
+        containerNode.querySelector('.button-delete-cancel').addEventListener('click', function(e) {
+            var bookId = parseInt(e.srcElement.dataset.bookId);
+            self.switchToDisplayMode(bookId);
+        });
+        return containerNode;
+    };
+
+    _bdp.renderTemplate = function(templateName, book) {
+        var template = document.getElementById(templateName).innerHTML;
+        var html = template
+                        .replace(/{{book.name}}/g, book.name)
+                        .replace(/{{book.id}}/g, book.id);
+        var resultNode = document.createElement('span');
+        resultNode.innerHTML = html;
+        return resultNode; 
+    };
+
+    _bdp.appendNewBook = function() {
+        var self = this;
+        var newBookName = document.getElementById('new-book-name').value.trim();
+        if (newBookName === '') {
+            alert('Book name cannot be empty');
+            return;
+        }
+        if (!self._bookStore.bookNameIsAvailable(newBookName)) {
+            alert('The Book is already in the directory');
+            return;
+        }
+        var newBook = {name: newBookName};
+        self._bookStore.addBook(newBook);
+        document.getElementById('new-book-name').value = '';
+    };
+
+    _bdp.setupAddBookListener = function() {
+        var self = this;
+        document.getElementById('bs-add-book-form').addEventListener('submit', function(evt) {
+            evt.preventDefault();
+            self.appendNewBook();
+        });
+    };
+
+    _bdp.run = function() {
+        var self = this;
+        self.setupAddBookListener();
+        self.showBookList();
+    };
+
+    return BookDirectoryFacade;
+});
--- js/book-store.js
+++ js/book-store.js
@@ -1,136 +1,135 @@
 define(['event-emitter'], function(EventEmitter) {
-"use strict";
+    "use strict";
 
-function BookStore() {
-    this._books = [];
-    this._eventEmitter = new EventEmitter();
-}
-
-var _bsp = BookStore.prototype;
-
-_bsp.containsBook = function(book) {
-    if (this.bookNameIsAvailable(book.name)) {
-        return false;
-    } else {
-        return true;
+    function BookStore() {
+        this._books = [];
+        this._eventEmitter = new EventEmitter();
     }
-};
 
-_bsp.bookNameIsAvailable = function(bookName, exceptionId) {
-    for (var bookIndex in this._books) {
-        var knownBook = this._books[bookIndex];
-        if (knownBook.name.toUpperCase() === bookName.toUpperCase()) {
-            if (knownBook.id !== exceptionId) {
-                return false;
+    var _bsp = BookStore.prototype;
+
+    _bsp.containsBook = function(book) {
+        if (this.bookNameIsAvailable(book.name)) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    _bsp.bookNameIsAvailable = function(bookName, exceptionId) {
+        for (var bookIndex in this._books) {
+            var knownBook = this._books[bookIndex];
+            if (knownBook.name.toUpperCase() === bookName.toUpperCase()) {
+                if (knownBook.id !== exceptionId) {
+                    return false;
+                }
             }
         }
-    }
-    return true;
-};
+        return true;
+    };
 
-_bsp.addBook = function(book) {
-    if (!this._isBook(book)) {
-        throw 'Book required';
-    }
-    if (this.containsBook(book)) {
-        /* Ignore a book with known name */
-        throw 'The book is already in the storage';
-    }
-    book.id = this._generateBookId();
-    this._books.push(book);
-    this._eventEmitter.emitEvent('book-added', book);
-    return book;
-};
-
-_bsp._generateBookId = function() {
-    var maxBookId = 0;
-    this._books.forEach(function(book) {
-        if (book.id > maxBookId) {
-            maxBookId = book.id;
+    _bsp.addBook = function(book) {
+        if (!this._isBook(book)) {
+            throw 'Book required';
         }
-    });
-    return maxBookId + 1;
-};
-
-_bsp.removeBook = function(book) {
-    if (!this._isBook(book)) {
-        throw 'Book required';
-    }
-    for (var bookIndex in this._books) {
-        var knownBook = this._books[bookIndex];
-        if (knownBook.id === book.id) {
-            this._books.splice(bookIndex, 1);
-            this._eventEmitter.emitEvent('book-deleted', book);
-            return book;
+        if (this.containsBook(book)) {
+            /* Ignore a book with known name */
+            throw 'The book is already in the storage';
         }
-    }
-    throw 'The book is not found';
-};
+        book.id = this._generateBookId();
+        this._books.push(book);
+        this._eventEmitter.emitEvent('book-added', book);
+        return book;
+    };
 
-_bsp.getAllBooks = function() {
-    return this._books.slice(0);
-};
+    _bsp._generateBookId = function() {
+        var maxBookId = 0;
+        this._books.forEach(function(book) {
+            if (book.id > maxBookId) {
+                maxBookId = book.id;
+            }
+        });
+        return maxBookId + 1;
+    };
 
-_bsp.findBooksByName = function(name) {
-    var foundBooks = [];
-    this._books.forEach(function(knownBook) {
-        if (knownBook.name.toUpperCase().indexOf(name.toUpperCase()) > -1) {
-            foundBooks.push(knownBook);
+    _bsp.removeBook = function(book) {
+        if (!this._isBook(book)) {
+            throw 'Book required';
         }
-    });
-    return foundBooks;
-};
-
-_bsp.getBookById = function(bookId) {
-    for (var bookIndex in this._books) {
-        var knownBook = this._books[bookIndex];
-        if (knownBook.id === bookId) {
-            return knownBook;
+        for (var bookIndex in this._books) {
+            var knownBook = this._books[bookIndex];
+            if (knownBook.id === book.id) {
+                this._books.splice(bookIndex, 1);
+                this._eventEmitter.emitEvent('book-deleted', book);
+                return book;
+            }
         }
-    }
-    throw 'The book is not found';
-};
+        throw 'The book is not found';
+    };
 
-_bsp.getEventEmitter = function() {
-    return this._eventEmitter;
-};
+    _bsp.getAllBooks = function() {
+        return this._books.slice(0);
+    };
 
-_bsp._isArray = function(argument) {
-    return Array.isArray(argument);    
-};
+    _bsp.findBooksByName = function(name) {
+        var foundBooks = [];
+        this._books.forEach(function(knownBook) {
+            if (knownBook.name.toUpperCase().indexOf(name.toUpperCase()) > -1) {
+                foundBooks.push(knownBook);
+            }
+        });
+        return foundBooks;
+    };
 
-_bsp._isObject = function(argument) {
-    return (
-            (argument !== null) 
-            && 
-            (typeof argument === 'object')
-    );    
-};
+    _bsp.getBookById = function(bookId) {
+        for (var bookIndex in this._books) {
+            var knownBook = this._books[bookIndex];
+            if (knownBook.id === bookId) {
+                return knownBook;
+            }
+        }
+        throw 'The book is not found';
+    };
 
-_bsp._isNumber = function(argument) {
-    return (typeof argument === 'number');    
-};
+    _bsp.getEventEmitter = function() {
+        return this._eventEmitter;
+    };
 
-_bsp._isString = function(argument) {
-    return (typeof argument === 'string');    
-};
+    _bsp._isArray = function(argument) {
+        return Array.isArray(argument);    
+    };
 
-_bsp._isBook = function(argument) {
-    return (
-            this._isObject(argument) 
-            &&
-            (argument.name)
-    );    
-};
+    _bsp._isObject = function(argument) {
+        return (
+                (argument !== null) 
+                && 
+                (typeof argument === 'object')
+        );    
+    };
 
-_bsp._isStoredBook = function(argument) {
-    return (
-            this._isBook(argument) 
-            && 
-            (argument.id)
-    );    
-};
+    _bsp._isNumber = function(argument) {
+        return (typeof argument === 'number');    
+    };
 
-return BookStore;
+    _bsp._isString = function(argument) {
+        return (typeof argument === 'string');    
+    };
 
+    _bsp._isBook = function(argument) {
+        return (
+                this._isObject(argument) 
+                &&
+                (argument.name)
+        );    
+    };
+
+    _bsp._isStoredBook = function(argument) {
+        return (
+                this._isBook(argument) 
+                && 
+                (argument.id)
+        );    
+    };
+
+    return BookStore;
 });
